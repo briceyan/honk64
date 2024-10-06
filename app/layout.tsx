02afd20013f1918224cd34b814ba9023125877df
Brice <briceyan3.0@gmail.com>
2024-10-06 12:11:45 +0800

    Update site meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+  title: "Honk64",
+  description: "How geese honks on Farcaster!",
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -14,10 +14,7 @@ export default function RootLayout({ children }: { children: any }) {
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
       </head>
       <body>
         <MantineProvider theme={theme}>{children}</MantineProvider>
